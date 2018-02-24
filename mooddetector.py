38e8003bae47e48201eed0c5c23d36fa9e0920af
Anatoly Tverdovsky <antverdovsky@gmail.com>
2018-02-24 03:22:57 -0600

    Implement API Request with Local Image
    Modifies the existing commit so that the API request can be made with a local image rather than a URL path.

--- mooddetector.py
+++ mooddetector.py
@@ -4,7 +4,7 @@ import logging
 API_URL = "https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect?returnFaceId=true&returnFaceLandmarks=false"
 API_KEY_1 = "8cd2115e508b4f0c8997a6283e90f5f0"
 API_KEY_2 = "4cd7b83eae764e6cbf820b7193192e2c"
-API_FORMAT = "application/json"
+API_FORMAT = "application/octet-stream"
 
 def make_api_call(image):
     """
@@ -12,7 +12,7 @@ def make_api_call(image):
     and returns the raw JSON data from it.
 
     Args:
-        image (str): The URL of the image to be sent to the API.
+        image (bytearray): The bytearray representation of the image data.
 
     Returns:
         The JSON data returned by the API call. If the request fails, None is
@@ -24,13 +24,8 @@ def make_api_call(image):
     headers["Ocp-Apim-Subscription-Key"] = API_KEY_1
     headers["Content-Type"] = API_FORMAT
 
-    # Set the JSON field to store the URL of the image
-    json = {
-        "url": image
-    }
-
     # Make an HTTP request for the API
-    response = requests.request("post", API_URL, headers=headers, json=json)
+    response = requests.request("post", API_URL, headers=headers, data=image)
 
     # Print HTTP response code and data
     print(response.status_code)
@@ -38,5 +33,9 @@ def make_api_call(image):
 
 if __name__ == "__main__":
     # Test API on simple image with a humanoid
-    TEST_IMAGE = "https://scontent-ort2-1.xx.fbcdn.net/v/t34.0-12/28313695_1635841079808552_878984412_n.jpg?oh=2124924de94bf3aa43308efde86de1a4&oe=5A939EB6"
-    make_api_call(TEST_IMAGE)
+    TEST_IMAGE_PATH = "res/deepu_bee.jpg"
+    test_image_data = None
+    with open(TEST_IMAGE_PATH, "rb") as f:
+        test_image_data = f.read()
+    
+    make_api_call(test_image_data)
