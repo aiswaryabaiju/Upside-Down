72ce99e76a0ade9545d963b2ce1fe728a7fde808
Abdul Rehman <abudrehman45@gmail.com>
2018-02-24 04:12:33 -0600

    added the capture portion and the retake portion of the CamFeed

--- Cam.py
+++ Cam.py
@@ -1,12 +1,20 @@
 import numpy as np
 import cv2
 
-
 def CamFeed():
+    """
+    This function takes feed from the front facing camera and displays it in
+    the screen, it takes in keyboard input; 'q' for quit (saves the image if 
+    image is frozen), 's' to freeze the current frame, and 'r' to unfreeze the
+    frame to retake picture.
+
+    Return:
+        gives back a True or false depending if the image was captured or not 
+    """
     #creates an instance of the camera
     cap = cv2.VideoCapture(0)
     while(True):
-        
+
         # Capture frame-by-frame
         ret, frame = cap.read()
 
@@ -16,14 +24,30 @@ def CamFeed():
         #if user presses 'q' quit the program
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
+        #if user presses 's' captures the picture and
+        #freezes it on the screen
+        elif cv2.waitKey(1) & 0xFF == ord('s'):
+            while (True):
+                #if user presses 'q' then the frame is saved
+                #to a file 
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    cv2.imwrite('tst.jpeg', frame)
+                    cap.release()
+                    cv2.destroyAllWindows()
+                    return True
+                #if the user presser 'r' it tries to retake the
+                #picture
+                elif cv2.waitKey(1) & 0xFF == ord('r'):
+                    break
             
         # When everything done, release the capture
     cap.release()
     cv2.destroyAllWindows()
+    return False
 
-
+#NEED TO DELETE BELOW THIS THE FUTURE
 def main():
-    CamFeed()
+    print(CamFeed())
 
 if __name__ == "__main__":
     main()
