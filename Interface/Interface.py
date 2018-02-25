43d827f3138eb3a264f3e9db0c8a1ec2688c76db
Abdul Rehman <abudrehman45@gmail.com>
2018-02-24 19:12:29 -0600

    make 3 pages

--- Interface/Interface.py
+++ Interface/Interface.py
@@ -8,26 +8,26 @@ class Window(tk.Tk):
     
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
-        container = tk.Frame(self)
-        container.pack(side='top', fill='both', expand=True)
+        tk.Tk.wm_title(self, 'Upside-Down')
 
+        container = tk.Frame(self)   
+        container.pack(side='top', fill='both', expand=True)
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
 
+
         self.frames = {}
 
-        frame = StartPage(container, self)
-
-        self.frames[StartPage] = frame
-        frame.grid(row=0, column=0,sticky='nsew')
+        for F in (StartPage, cam_page, result_page):
+            frame = F(container, self)
+            self.frames[F] = frame
+            frame.grid(row=0, column=0,sticky='nsew')
         self.show_frame(StartPage)
     
     def show_frame(self, cont):
         frame = self.frames[cont]
         frame.tkraise()
 
-def qf():
-    print ("HELLO\n")
 
 class StartPage(tk.Frame):
 
@@ -37,7 +37,30 @@ class StartPage(tk.Frame):
         label.pack(pady=10, padx=10)
 
         button1 = tk.Button(self, text='visit page 1', 
-                            command=None)
+                            command=lambda: controller.show_frame(cam_page))
         button1.pack()
+
+class cam_page(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self,text='Camera Page', font = LARGE_FONT)
+        label.pack(pady=10, padx=10)
+
+        button1 = tk.Button(self, text='Get Result', 
+                            command=lambda: controller.show_frame(result_page))
+        button1.pack()
+
+class result_page(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self,text='Result Page', font = LARGE_FONT)
+        label.pack(pady=10, padx=10)
+
+        button1 = tk.Button(self, text='Go home', 
+                            command=lambda: controller.show_frame(StartPage))
+        button1.pack()
+
 app = Window()
 app.mainloop()
